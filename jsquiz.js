b3003f19e531c6548f88ab31eff37718691f66a6
HCCMacBook <HCCMacBook@MacDaddy.dfci.harvard.edu>
2013-09-30 09:20:23 -0400

    Initial commit

---
+++ jsquiz.js
@@ -0,0 +1,31 @@
+var questions = [
+			{
+				question: "What is the capital of Russia?",
+				answers: ["Kathmandu", "Beijing", "Moscow", "Krakow"],
+				correct: "Moscow",
+			},
+			{
+				question: "How many legs does a spider have?",
+				answers: ["6", "8", "10", "12"],
+				correct: "8",
+			},
+			{
+				question: "What color tongue does a Sharpei have?",
+				answers: ["pink", "red", "black", "blue"],
+				correct: "black",
+			},
+			{
+				question: "What color dress is traditionally worn by an Indian bride?",
+				answers: ["red", "white", "pink", "yellow"],
+				correct: "red",
+			}
+		];
+
+	//console.log(questions[0].question);
+/*
+	for(var questionNum in questions){
+		var q = questions[questionNum];
+		console.log(q.question);
+		console.log(q.answers);
+	}
+	*/
